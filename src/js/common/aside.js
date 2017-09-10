e7f5f1394516d99b8539f351d022865371a55f0d
PengFei <626138959@qq.com>
2017-09-10 15:15:59 +0800

    左侧导航功能实现，首页与讲师列表也测试

--- src/js/common/aside.js
+++ src/js/common/aside.js
@@ -15,3 +15,23 @@ var userinfoStr = localStorage.getItem('userinfo');
 var userinfo = JSON.parse(userinfoStr);
 $('.aside img').attr('src', userinfo.tc_avatar);
 $('.aside h4').text(userinfo.tc_name);
+
+/**
+ * 点击标题子列表显示隐藏：
+ * 1、获取导航中a标签绑定点击事件
+ * 2、事件触发时让它的下一个兄弟元素ul显示隐藏切换
+ * */
+$('.navs a').on('click', function() {
+  $(this).next('ul').slideToggle();
+});
+
+/**
+ * 根据访问的页面给对应的标题添加焦点：
+ * 1、首先获取页面location.pathname
+ * 2、获取全部的导航a标签，先统一取出active类名
+ * 3、然后利用这个值和导航a标签的href去匹配得到对应的a标签，添加active类名焦点样式
+ * 4、最后在焦点a标签的父级元素，让它show
+ * */
+var path = location.pathname;
+$('.navs a').removeClass('active');
+$('.navs a[href="' + path + '"]').addClass('active').parents('ul').show();
