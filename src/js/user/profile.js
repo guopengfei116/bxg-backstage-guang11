35e259514dacd0a8effb5b0918c803b357f82d93
PengFei <626138959@qq.com>
2017-09-10 20:53:09 +0800

    个人中心数据提交，修改密码

--- src/js/user/profile.js
+++ src/js/user/profile.js
@@ -4,7 +4,7 @@ require('../common/aside.js');
 /**
  * 该页面功能点：
  * 1、数据回显
- * 2、表单提交
+ * 2、
  */
 
 /**
@@ -22,4 +22,19 @@ $.ajax({
             $('.teacher-profile').html(template('teacher-profile-tpl', data.result));
         }
     }
+});
+
+/**
+ * 表单提交：
+ * 1、因为表单要进行数据回显，所以是动态异步创建出来的。
+ * 我们这里要通过插件的ajaxForm监听表单提交事件必须使用委托的方式，插件提供了delegation选项配置为true即可。
+ * 2、修改成功后给个用户提示
+ * */
+$('#teacher-profile-form').ajaxForm({
+  delegation: true,
+  success: function(data) {
+    if(data.code == 200) {
+      alert('修改成功');
+    }
+  }
 });
--- src/js/user/repass.js
+++ src/js/user/repass.js
@@ -1,2 +1,29 @@
 require('../common/header.js');
 require('../common/aside.js');
+
+/**
+ * 修改密码：
+ * 1、这里我们要在表单提交前做一个校验
+ * 2、需要使用更灵活的ajaxSubmit方法，我们额外手动监听下表单事件
+ * */
+$('#repass-form').on('submit', function() {
+
+  // 两次新密码校验，如果密码一致了，那么转ajax提交
+  if($('#input-pass').val() !== $('#input-pass-repeat').val()) {
+    alert('两次密码输入不一致');
+    return false;
+  }
+
+  // 这个方法只负责获取表单中的值，发送ajax请求，并不负责表单事件的监听与阻止，
+  // 即时调即时发送请求，调用这个方法就相当于调用$.ajax
+  // 之前的那个方法比较强大，但是不灵活，这个相对弱一点，但是比较灵活。
+  $('#repass-form').ajaxSubmit({
+    success: function(data) {
+      console.log(data);
+    }
+  });
+
+  return false;
+});
+
+
