2df57aab01c50e4147372e40ecf435659a026a4f
PengFei <626138959@qq.com>
2017-09-11 10:36:55 +0800

    学科分类列表实现

--- src/js/category/list.js
+++ src/js/category/list.js
@@ -1,2 +1,11 @@
 require('../common/header.js');
 require('../common/aside.js');
+
+/**
+ * 渲染学科列表：
+ * 1、请求接口获取列表数据
+ * 2、使用模版引擎得到数据渲染后的html，插入到页面指定位置
+ * */
+$.get('/v6/category', function(data) {
+  $('.table-bordered').append(template('category-list-tpl', data.result));
+});
