06a0aaad02e2aed850181602e64d470aac20448b
PengFei <626138959@qq.com>
2017-09-13 18:27:33 +0800

    重新规划章节编辑添加代码组织

--- src/js/course/edit3.js
+++ src/js/course/edit3.js
@@ -4,16 +4,18 @@ var util = require('../common/util.js');
 
 /**
  * 功能点：
- * 1、数据回显
+ * 1、页面数据回显
  * 2、编辑章节
  *    2.1、数据回显
  *    2.2、数据提交
  * 3、添加章节
+ *    3.1、数据回显   --  按照常理是不需要回显的，但是因为和编辑同用html模版，需要回显空数据
+ *    3.2、数据提交
  * */
 var cs_id = util.getSearch('cs_id');
 
 /**
- * 数据回显：
+ * 页面数据回显：
  * 1、拿到location.search里面的cs_id
  * 2、通过这个id请求接口获取数据
  * 3、得到数据渲染后的模版，插入页面指定的位置
@@ -45,34 +47,34 @@ $(document).on('click', '.btn-lesson-edit', function() {
 });
 
 /**
- * 编辑章节_表单提交：
- * 1、因为表单要数据回显是动态生成的，所以使用ajaxForm插件方法的委托
- * 2、成功后给个提示
- * */
-$('#lesson-form').ajaxForm({
-  delegation: true,
-  success: lessonSuccess
-});
-
-/**
- * 添加章节：
- * 1、因为表单要数据回显是动态生成的，所以使用委托的方式给添加按钮绑定click事件
- * 2、事件触发时使用一个空对象渲染模态框模版，插入到页面中
+ * 添加章节_数据回显
+ * 1、因为页面是动态生成的，所以需要通过委托的方式给添加按钮绑定click事件
+ * 2、因为模态框中的内容做成了模版，所以使用一个空对象渲染模版，插入到页面中
  * */
 $(document).on('click', '#btn-lesson-add', function() {
+  // 空对象中额外添加cs_id是因为后端需要这个值来区分修改的章节属于那个课程
   $('#chapterModal').html(template('lesson-tpl', { cs_id: cs_id }));
 });
 
 /**
- * 修改或添加章节成功后的处理回调：
- * 1、如果服务器返回的数据中有result那么证明是添加章节成功了，否则就是修改成功了
- * 2、不同的情况给出不同的用户提示，然后添加章节成功后清空表单
+ * 修改章节与添加章节：
+ * 1、因为在同一个页面中，修改与添加使用的是同一个form，这里一起就处理了
+ * 2、通过ajaxForm插件方法把表单默认刷新提交转ajax提交，因为form是动态生成的，所以要使用委托
+ * 3、请求成功后，通过判断服务器data.result来却分是修改还是添加，给用户不同的提示信息
  * */
-function lessonSuccess(data) {
-  if(data.result) {
-    alert('添加成功');
-    $('#lesson-form').get(0).reset();
-  }else {
-    alert('修改成功');
+$('#lesson-tpl').ajaxForm({
+  delegation: true,
+  success: function(data) {
+
+    // 添加成功后，给出提示，并重置表单
+    if(data.result) {
+      alert('添加成功');
+      $('#lesson-tpl').get(0).reset();
+    }
+
+    // 修改成功后，给出提示
+    else {
+      alert('修改成功');
+    }
   }
-}
+});
