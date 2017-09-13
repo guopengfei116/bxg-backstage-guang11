76482834f2188c20d25e10d7b984a67535d0bce2
PengFei <626138959@qq.com>
2017-09-13 16:01:22 +0800

    课程编辑第三步数据回显

--- src/js/course/edit3.js
+++ src/js/course/edit3.js
@@ -1,2 +1,24 @@
 require('../common/header.js');
 require('../common/aside.js');
+var util = require('../common/util.js');
+
+/**
+ * 功能点：
+ * 1、数据回显
+ * 2、编辑章节
+ * 3、添加章节
+ * */
+var cs_id = util.getSearch('cs_id');
+
+/**
+ * 数据回显：
+ * 1、拿到location.search里面的cs_id
+ * 2、通过这个id请求接口获取数据
+ * 3、得到数据渲染后的模版，插入页面指定的位置
+ * */
+$.get('/v6/course/lesson', { cs_id: cs_id }, function(data) {
+  if(data.code == 200) {
+    data.result.editIndex = 3;
+    $('#course-edit3').append(template('course-edit3-tpl', data.result));
+  }
+});
