6f549109f1022fd0fe033080d25f35408d2859a3
PengFei <626138959@qq.com>
2017-09-13 11:44:50 +0800

    课程编辑第一步数据提交

--- src/js/course/edit1.js
+++ src/js/course/edit1.js
@@ -5,8 +5,8 @@ var util = require('../common/util.js');
 /**
  * 功能点：
  * 1、数据回显
- * 2、表单提交
- * 3、学科二级联动
+ * 2、学科二级联动
+ * 3、表单提交
  * */
 var cs_id = util.getSearch('cs_id');
 
@@ -19,5 +19,49 @@ var cs_id = util.getSearch('cs_id');
 $.get('/v6/course/basic', { cs_id: cs_id }, function(data) {
   if(data.code == 200) {
     $('#course-edit1').append(template('course-edit1-tpl', data.result));
+  }
+});
+
+/**
+ * 学科二级联动：
+ * 1、因为整个数据回显是动态构建的，所以通过委托的方式监听父级学科select的change事件
+ * 2、事件发生时获取所选顶级学科cg_id，调用接口获取对应的子级学科列表
+ * 3、拿到数据后动态生成新的子级option进行替换
+ * */
+$(document).on('change', '#category-top-select', function() {
+
+  // select的value就是用户所选的顶级学科cg_id
+  var topCgid = $(this).val();
+
+  // 利用顶级学科cg_id获取对应子级学科列表
+  $.get('/v6/category/child', { cg_id: topCgid }, function(data) {
+
+    var html = '';
+    var childList = data.result;
+
+    if(data.code == 200) {
+      // 根据子级列表动态生成对应的option
+      for(var i = 0, len = childList.length; i < len; i++) {
+        html += '<option value="' + childList[i].cg_id + '">' + childList[i].cg_name + '</option>'
+      }
+    }
+
+    // 使用新的option进行替换
+    $('#category-child-select').html(html);
+  });
+});
+
+/**
+ * 表单提交：
+ * 1、使用ajaxForm方法的委托方式表单转ajax
+ * 2、编辑成功后给出提示，然后跳转到编辑第二步页面(跳转时需要继续把cs_id传递过去)
+ * */
+$('#course-edit1-form').ajaxForm({
+  delegation: true,
+  success: function(data) {
+    if(data.code == 200) {
+      alert('修改成功');
+      location.href = '/dist/html/course/edit2.html?cs_id=' + cs_id;
+    }
   }
 });
