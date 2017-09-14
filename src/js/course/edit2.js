c00ee4c5a7a14bbd5d671a519f9cff8b5a6fd806
PengFei <626138959@qq.com>
2017-09-14 21:37:51 +0800

    封面图上传

--- src/js/course/edit2.js
+++ src/js/course/edit2.js
@@ -22,5 +22,38 @@ $.get('/v6/course/picture', { cs_id: cs_id }, function(data) {
   if(data.code == 200) {
     data.result.editIndex = 2;
     $('#course-edit2').append(template('course-edit2-tpl', data.result));
+    initPlugin();
   }
+});
+
+/**
+ * 初始化插件
+ * */
+function initPlugin() {
+
+  $('#uploadify').uploadify({
+    swf: '/lib/jquery-uploadify/uploadify.swf',  // 这个是flash脚本，必须引入，不然无法选择文件
+    uploader: '/v6/uploader/cover',                  // 这个是上传图片的接口
+    fileTypeExts: '*.gif; *.jpg; *.png',                 // 这个用来限制上传图片的类型
+    fileObjName: 'cs_cover_original',                // 这个用来设置提交给后端时，文件数据对应的name
+    formData: {                                                  // 如果接口需要额外的数据，通过这个配置来添加
+      cs_id:  cs_id
+    },
+    buttonText: '上传',
+    buttonClass: 'btn btn-success btn-sm',
+    onUploadSuccess: function(file, dataStr) {                    // 图片上传成功后的回调
+      var data = JSON.parse(dataStr);
+      $('.preview img').attr('src', data.result.path);
+    }
+  });
+
+}
+
+
+
+/**
+ * 委托方式给裁剪图片绑定点击事件，初始化裁剪插件
+ * */
+$(document).on('click', '#btn-clip', function() {
+
 });
